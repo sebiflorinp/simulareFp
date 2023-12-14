568386534cb816ee06b6ca212e67fd81c7a4c4c4
sebifp <sebiflorinp@gmail.com>
2023-12-14 09:17:55 +0200

    FIX date not showing in the right format

--- domain/objects.py
+++ domain/objects.py
@@ -1,5 +1,12 @@
 class Suspect:
 	def __init__(self, id, description, date, type, name):
+		"""
+		The constructor of the Suspect class.
+		Preconditions: id: a positive integer
+									 description: a string
+									 date: a datetime object
+									 
+		"""
 		self.__id = id
 		self.__description = description
 		self.__date = date
@@ -7,7 +14,7 @@ class Suspect:
 		self.__name = name
 	
 	def __str__(self):
-		return f"{self.__id},{self.__description},{self.__date},{self.__date},{self.__type},{self.__name}"
+		return f"{self.__id},{self.__description},{self.__date.year}/{self.__date.month}/{self.__date.day},{self.__type},{self.__name}"
 	
 	def getId(self):
 		return self.__id
